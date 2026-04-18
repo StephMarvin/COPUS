0193c45d65daaf227c7308d02be320f96ea2bc01
Marvin M. Agudines <140509436+StephMarvin@users.noreply.github.com>
2026-04-18 18:03:19 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> COPUS: Select User </title>
+
+  <link rel="shortcut icon" type="image/x-icon" href="public/assets/website-logo-cite.png" />
+  
+  <link rel="stylesheet" href="public/login/assets/css/styles.min.css" />
+  <link rel="stylesheet" href="public/login/assets/css/custom-styles.css" />
+</head>
+
+<body>
+
+  <!-- Main -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
+    <div class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+      <div class="d-flex align-items-center justify-content-center w-100">
+        <div class="row justify-content-center w-100">
+
+          <div class="col-md-8 col-lg-6 col-xxl-3">
+
+            <div class="card mb-0">
+
+              <div class="card-body">
+                <a href="index.php" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                  <img src="public/assets/website-logo-cite.png" width="180" alt="">
+                </a>
+                <p class="text-center"> Select Your Account Type </p>
+                
+                <div class="container mb-2">
+
+                  <div class="row flex-column gap-2">
+
+                    <a href="index.php" class="btn btn-primary custom-btn">
+                      Go to Main Page
+                    </a>
+
+                    <a href="pages/admin/login.php" class="btn btn-primary admin-btn">
+                      Administrator
+                    </a>
+
+                    <a href="pages/deans/login.php" class="btn btn-primary dean-btn">
+                      Dean
+                    </a>
+
+                    <a href="pages/observer/login.php" class="btn btn-warning">
+                      Observer
+                    </a>
+
+                    <a href="pages/teacher/login.php" class="btn btn-primary">
+                      Teacher
+                    </a>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Main -->
+
+</body>
+
+</html>
